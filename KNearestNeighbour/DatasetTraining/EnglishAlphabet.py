e7e9d8852a12bb95d4c0bad1fb74073b2379f4d9
AlphaBAT69 <tanaytoshniwal98@gmail.com>
2017-10-08 16:10:09 +0530

    Optical Character Recognition using k-Nearest-Neighbour

---
+++ KNearestNeighbour/DatasetTraining/EnglishAlphabet.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+img = cv2.imread('/home/alphabat69/OpenCV/samples/data/digits.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
+x = np.array(cells)
+train = x[:,:50].reshape(-1,400).astype(np.float32)
+test = x[:,50:100].reshape(-1,400).astype(np.float32)
+k = np.arange(10)
+train_labels = np.repeat(k,250)[:,np.newaxis]
+test_labels = train_labels.copy()
+knn = cv2.ml.KNearest_create()
+knn.train(train, cv2.ml.ROW_SAMPLE, train_labels)
+ret,result,neighbours,dist = knn.findNearest(test,k=5)
+matches = result==test_labels
+correct = np.count_nonzero(matches)
+accuracy = correct*100.0/result.size
+print( accuracy )
+np.savez('knn_data_englishalphabet.npz',train=train, train_labels=train_labels)
+with np.load('knn_data_englishalphabet.npz') as data:
+    print( data.files )
+    train = data['train']
+    train_labels = data['train_labels']
+    
